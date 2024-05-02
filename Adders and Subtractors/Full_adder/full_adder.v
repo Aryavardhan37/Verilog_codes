363c837c592c524358a002e7bdaba11b61a25755
Aryavardhan Sharma <aryavardhansharma37@gmail.com>
2024-05-03 04:38:03 +0530

    Update full_adder.v

--- Adders and Subtractors/Full_adder/full_adder.v
+++ Adders and Subtractors/Full_adder/full_adder.v
@@ -1,6 +1,16 @@
-module full_adder(a, b, cin,sum, cout);
-	input a,b,cin;
-	output sum,cout;
-   	assign sum = a ^ b ^ cin;
-   	assign cout = (a & b) | (cin & (a ^ b));
+module full_adder (
+    input a, b, cin,
+    output sum, carry
+);
+
+    wire w1, w2, w3, w4;
+
+    xor w1 (sum, a, b);
+    and w2 (w2, a, b);
+    and w3 (w3, b, cin);
+    and w4 (w4, cin, a);
+
+    or or_gate (carry, w2, w3, w4);
+
 endmodule
+
